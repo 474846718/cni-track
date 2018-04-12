8e8c2b8a9d12031b2ac1bb79cb2317a7aa6aa43c
qq474 <474846718@qq.com>
2018-04-12 16:06:55 +0800

    能直接使用 先maven install所有依赖 再maven package所有jar

--- track-core/src/test/java/com/cni/TrackCoreApplicationTests.java
+++ track-core/src/test/java/com/cni/TrackCoreApplicationTests.java
@@ -4,17 +4,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TrackCoreApplicationTests {
 
-	@Autowired
-
-
 	@Test
 	public void contextLoads() {
+		StringHttpMessageConverter stringHttpMessageConverter;
 	}
 
 }
